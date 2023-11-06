0aad9b6fcf68908527e07e3242cb1e970c48f0ff
sararaza410 <sara.raza@xprolabs.com>
2023-11-06 17:53:49 +0500

    Fixed an Issue with Prevention Controller

--- app/controllers/api/v2/preventions_controller.rb
+++ app/controllers/api/v2/preventions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Main API controller for Prevention Components Records
-class Api::V2::PreventionsController < ApplicationController
+class Api::V2::PreventionsController < ApplicationApiController
   include Api::V2::Concerns::Pagination
   include Api::V2::Concerns::Record
 
