cdc756958c1395c0e7d8f899f399117cd9ffbdb6
sararaza410 <sara.raza@xprolabs.com>
2023-11-06 19:25:53 +0500

    Fixed Issues with Prevention Model attributes. Need to add proper fields to FrontEnd.

--- app/models/prevention.rb
+++ app/models/prevention.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class Prevention < ApplicationRecord
-  # Maybe Used for searching somethin
-  NAME_FIELDS = %w[name name_nickname name_other].freeze
-
   # Defines shared functionality and characteristics for all core Primero record types, enhancing code consistency and maintainability.
   include Record
   # Provides functionality for indexing and searching record fields using the Sunspot search library, including handling different field types and custom indexing configurations.
@@ -25,9 +22,6 @@ class Prevention < ApplicationRecord
   include Attachable
   # Enhances database query efficiency by enabling eager loading of specified associations for records.
   include EagerLoadable
-  # Enables records to interact with external systems using webhooks, offering support for managing webhook-related
-  # attributes and tracking synchronization status. An essential tool for real-time data exchange.
-  include Webhookable
   # Simplifies location service integration in records by providing an attribute writer to set the location service
   # and a method to access the LocationService singleton. Essential for efficient location-based operations in records.
   include LocationCacheable
@@ -37,396 +31,84 @@ class Prevention < ApplicationRecord
   # Methods to Calculate Stats for Graphs
   extend PreventionGraphs
 
-  # TODO Add the needed Form Fields or Custom Accessors
   # Accessors
   store_accessor(
     :data,
     :prevention_id,
+    :registration_date,
     :prevention_id_display,
-    :unique_id,
+    :name,
+    :hidden_name,
+    :module_id,
+    :location_current,
+    # Prevention Forms
+    :influencer_mapping,
+    :details_of_meetings,
+    :community_engagement_sessions,
     :male_community_based_child_protection_committees,
     :female_community_based_child_protection_committees,
-    :community_engagement_sessions
+    :capacity_building_training_or_workshop_received,
+    :awareness_session_conducted_in_the_community,
+    :community_mobilization,
+    :community_based_child_protection_committee_training,
+    :capacity_building_training_or_workshop_delivered,
+    :event_celebrations,
   )
-
-
-  # TODO Filter form this and see what is actually needed or needs to be added or modified
-  # Would need to add all the columns that will be added as keys in the data jsonb column of the child model.
-  # Or will they be added to the prevention records themselfs
-
-  # store_accessor(
-  #   :data,
-  #   :case_id, :case_id_code, :case_id_display,
-  #   :nickname, :name, :protection_concerns, :consent_for_tracing, :hidden_name,
-  #   :name_first, :name_middle, :name_last, :name_nickname, :name_other,
-  #   :registration_date, :age, :estimated, :date_of_birth, :sex, :address_last,
-  #   :risk_level, :date_case_plan, :case_plan_due_date, :date_case_plan_initiated,
-  #   :date_closure, :assessment_due_date, :assessment_requested_on,
-  #   :followup_subform_section, :protection_concern_detail_subform_section,
-  #   :disclosure_other_orgs,
-  #   :ration_card_no, :icrc_ref_no, :unhcr_id_no, :unhcr_individual_no, :un_no, :other_agency_id,
-  #   :survivor_code_no, :national_id_no, :other_id_no, :biometrics_id, :family_count_no, :dss_id, :camp_id, :cpims_id,
-  #   :tent_number, :nfi_distribution_id,
-  #   :nationality, :ethnicity, :religion, :language, :sub_ethnicity_1, :sub_ethnicity_2, :country_of_origin,
-  #   :displacement_status, :marital_status, :disability_type, :incident_details,
-  #   :location_current, :tracing_status, :name_caregiver,
-  #   :registry_id_display, :registry_name, :registry_no, :registry_location_current,
-  #   :urgent_protection_concern, :child_preferences_section, :family_details_section, :care_arrangements_section,
-  #   :duplicate, :cp_case_plan_subform_case_plan_interventions, :has_case_plan
-  # )
-
-  # Associations
-    # TODO Figureout the associations for this model
-  #
-  # Maybe there needs to be foreing key column too to related this table to the Child model
-  # has_many   :incidents      ,                      foreign_key: :incident_case_id
-  # has_many   :matched_traces , class_name: 'Trace', foreign_key: 'matched_case_id'
-  # has_many   :duplicates     , class_name: 'Child', foreign_key: 'duplicate_case_id'
-  # belongs_to :duplicate_of   , class_name: 'Child', foreign_key: 'duplicate_case_id', optional: true
-  # belongs_to :registry_record,                      foreign_key: :registry_record_id, optional: true
-
-
-  # # may be add the filed for Prevention forms now.
-  # def self.sortable_text_fields
-  #   %w[name case_id_display national_id_no registry_no]
-  # end
-
-  # def self.filterable_id_fields
-  #   # The fields family_count_no and dss_id are hacked in only because of Bangladesh
-  #   # The fields camp_id, tent_number and nfi_distribution_id are hacked in only because of Iraq
-  #   %w[ unique_identifier short_id case_id_display case_id
-  #       ration_card_no icrc_ref_no rc_id_no unhcr_id_no unhcr_individual_no un_no
-  #       other_agency_id survivor_code_no national_id_no other_id_no biometrics_id
-  #       family_count_no dss_id camp_id tent_number nfi_distribution_id oscar_number registry_no ]
-  # end
-
-  # # Maybe Used for Searching using Searchable do Block
-  # def self.quicksearch_fields
-  #   filterable_id_fields + NAME_FIELDS
-  # end
-
-  # # use this too
-  # def self.summary_field_names
-  #   common_summary_fields + %w[
-  #     case_id_display name survivor_code_no age sex registration_date
-  #     hidden_name workflow case_status_reopened module_id registry_record_id
-  #   ]
-  # end
-
-  # def self.alert_count_self(current_user)
-  #   records_owned_by = open_enabled_records.owned_by(current_user.user_name).ids
-  #   # TODO: Once relation between transition and record is fixee, use joins(:transitions)
-  #   records_referred_users = open_enabled_records.joins(
-  #     "INNER JOIN transitions ON transitions.record_type = 'Child' AND (transitions.record_id)::uuid = cases.id"
-  #   ).where(transitions:
-  #     {
-  #       type: Referral.name,
-  #       status: [Transition::STATUS_INPROGRESS, Transition::STATUS_ACCEPTED],
-  #       transitioned_to: current_user.user_name
-  #     }).ids
-  #   (records_referred_users + records_owned_by).uniq.count
-  # end
-
-  # def self.child_matching_field_names
-  #   MatchingConfiguration.matchable_fields('case', false).pluck(:name) | MatchingConfiguration::DEFAULT_CHILD_FIELDS
-  # end
-
-  # def self.family_matching_field_names
-  #   MatchingConfiguration.matchable_fields('case', true).pluck(:name) | MatchingConfiguration::DEFAULT_INQUIRER_FIELDS
-  # end
-
-  # def self.api_path
-  #   '/api/v2/cases'
-  # end
-
-  # searchable do
-  #   filterable_id_fields.each { |f| string( "#{f}_filterable", as: "#{f}_filterable_sci") { data[f] } }
-  #   sortable_text_fields.each { |f| string( "#{f}_sortable"  , as: "#{f}_sortable_sci"  ) { data[f] } }
-
-  #   Child.child_matching_field_names.each { |f| text_index(f, suffix: 'matchable') }
-  #   Child.family_matching_field_names.each do |f|
-  #     text_index(f, suffix: 'matchable', subform_field_name: 'family_details_section')
-  #   end
-
-  #   quicksearch_fields.each { |f| text_index(f) }
-
-  #   %w[registration_date date_case_plan_initiated assessment_requested_on date_closure].each { |f| date(f) }
-  #   %w[estimated urgent_protection_concern consent_for_tracing has_case_plan].each do |f|
-  #     boolean(f) { data[f] == true || data[f] == 'true' }
-  #   end
-  #   %w[day_of_birth age].each { |f| integer(f) }
-  #   %w[id status sex current_care_arrangements_type].each { |f| string(f, as: "#{f}_sci") }
-
-  #   string :risk_level, as: 'risk_level_sci' do
-  #     risk_level.present? ? risk_level : RISK_LEVEL_NONE
-  #   end
-  #   string :protection_concerns, multiple: true
-
-  #   date(:assessment_due_dates, multiple: true) { Tasks::AssessmentTask.from_case(self).map(&:due_date) }
-  #   date(:case_plan_due_dates , multiple: true) { Tasks::CasePlanTask.from_case(self).map(&:due_date) }
-  #   date(:followup_due_dates  , multiple: true) { Tasks::FollowUpTask.from_case(self).map(&:due_date) }
-
-  #   boolean(:has_incidents) { incidents.size.positive? }
-
-  #   # Define a 'text' field for Sunspot indexing.
-  #   # This field extracts the value associated with the 'nationality_b80911e' key from the 'data' JSON object.
-  #   text :nationality_b80911e do
-  #     data["nationality_b80911e"]
-  #   end
-  # end
-
-
-  # Validations
-    # TODO Add validations
-  # validate :validate_date_of_birth
-
-  # Callbacks
-    # TODO Add Callbacks
-  # before_save   :sync_protection_concerns
-  # before_save   :auto_populate_name
-  # before_save   :stamp_registry_fields
-  # before_save   :calculate_has_case_plan
-  # before_create :hide_name
-  # after_save    :save_incidents
-
-  # The purpose of this method is to provide a list of fields that can be used to sort Child records
-    # in a text-based, alphabetical, or lexicographical order when necessary.
-  # For example, if you want to display a list of Child records sorted by their names or other text-based
-    # attributes, you would use these field names to sort the records effectively.
-  #
-  # TODO Add the relevant fields that can be used for Prevention records
-  def self.sortable_text_fields
-    %w[ ]
-  end
-
-  # TODO Add the relevant fields that can be used for Prevention records
-  # This defines a list of fields primarily used for filtering and searching, and
-  # it includes fields for unique identification and additional identifiers.
-  def self.filterable_id_fields
-    %w[ unique_identifier prevention_id ]
-  end
-
-  # TODO Add the relevant fields that can be used for Prevention records
-  # This combines the filterable identification fields with name-related fields to create a
-  # list of attributes that can be quickly searched when using the Searchable module.
-  def self.quicksearch_fields
-    filterable_id_fields + %w[ ]
-  end
-
-  # TODO Add the relevant fields that can be used for Prevention records
-  # This method is used to define a standard set of field names that should be included in the summary view of
-  # Prevention records. The FieldSelectionService class uses this method to determine which fields to display
-  # based on the query parameters and user role when generating responses for Prevention records.
-  def self.summary_field_names
-    common_summary_fields + %w[
-      id
-    ]
-  end
 
   # This string represents the endpoint or path that clients can use to interact with Prevention records in the API
   def self.api_path
     '/api/v2/preventions'
   end
 
-  searchable do
-    # This code iterates through the sortable text fields defined by self.sortable_text_fields and
-    # configures Sunspot to index and store those fields in a way that allows sorting.
-    # It adds a sortable text field for each of the specified attributes, which enables efficient sorting
-    # of Prevention records based on these fields during search operations.
-    sortable_text_fields.each { |f| string( "#{f}_sortable"  , as: "#{f}_sortable_sci"  ) { data[f] } }
+  class << self
+    def filterable_id_fields
+      %w[ prevention_id short_id ]
+    end
 
-    filterable_id_fields.each { |f| string("#{f}_filterable", as: "#{f}_filterable_sci") { data[f] } }
+    def quicksearch_fields
+      filterable_id_fields + %w[ name ]
+    end
 
-    quicksearch_fields.each { |f| text_index(f) }
+    def summary_field_names
+      common_summary_fields + %w[
+        prevention_id_display
+        name
+        registration_date
+        module_id
+        location_current
+      ]
+    end
 
-    %w[id].each { |f| string(f, as: "#{f}_sci") }
+    def sortable_text_fields
+      %w[ short_id name ]
+    end
   end
 
+  searchable do
+    %w[ id status ].each { |f| string(f, as: "#{f}_sci") }
+    %w[ registration_date ].each { |f| date(f) }
+    filterable_id_fields.each { |f| string("#{f}_filterable", as: "#{f}_filterable_sci") { data[f] } }
+    quicksearch_fields.each { |f| text_index(f) }
+    sortable_text_fields.each { |f| string("#{f}_sortable", as: "#{f}_sortable_sci") { data[f] } }
+  end
 
+  alias super_defaults defaults
+  def defaults
+    super_defaults
+    self.registration_date ||= Date.today
+  end
+
+  def self.report_filters
+    [
+      { 'attribute' => 'status', 'value' => [STATUS_OPEN] },
+      { 'attribute' => 'record_state', 'value' => ['true'] }
+    ]
+  end
 
   def set_instance_id
     self.prevention_id ||= unique_identifier
+    self.prevention_id_display ||= short_id
   end
-
-  def to_s
-    name.present? ? "#{name} (#{unique_identifier})" : unique_identifier
-  end
-
-  # class << self
-  #   alias super_new_with_user new_with_user
-  #   def new_with_user(user, data = {})
-  #     new_case = super_new_with_user(user, data).tap do |local_case|
-  #       local_case.registry_record_id ||= local_case.data.delete('registry_record_id')
-  #     end
-  #     new_case
-  #   end
-  # end
-
-  # alias super_defaults defaults
-  # def defaults
-  #   super_defaults
-  #   self.registration_date ||= Date.today
-  #   self.notes_section ||= []
-  # end
-
-  # def self.report_filters
-  #   [
-  #     { 'attribute' => 'status'      , 'value' => [STATUS_OPEN] },
-  #     { 'attribute' => 'record_state', 'value' => ['true']      }
-  #   ]
-  # end
-
-  # # TODO: does this need reporting location???
-  # # TODO: does this need the reporting_location_config field key
-  # # TODO: refactor with nested
-  # def self.minimum_reportable_fields
-  #   {
-  #     'boolean' => ['record_state'],
-  #     'string' => %w[
-  #       status sex risk_level owned_by_agency_id owned_by workflow workflow_status risk_level consent_reporting
-  #     ],
-  #     'multistring' => %w[associated_user_names associated_user_agencies owned_by_groups],
-  #     'date' => ['registration_date'],
-  #     'integer' => ['age'],
-  #     'location' => %w[owned_by_location location_current]
-  #   }
-  # end
-
-  # def self.nested_reportable_types
-  #   [ReportableProtectionConcern, ReportableService, ReportableFollowUp]
-  # end
-
-  # def validate_date_of_birth
-  #   return unless date_of_birth.present? && (!date_of_birth.is_a?(Date) || date_of_birth.year > Date.today.year)
-
-  #   errors.add(:date_of_birth, I18n.t('errors.models.child.date_of_birth'))
-  # end
-
-  # alias super_update_properties update_properties
-  # def update_properties(user, data)
-  #   build_or_update_incidents(user, (data.delete('incident_details') || []))
-  #   self.registry_record_id = data.delete('registry_record_id') if data.key?('registry_record_id')
-  #   self.mark_for_reopen = @incidents_to_save.present?
-  #   super_update_properties(user, data)
-  # end
-
-  # def build_or_update_incidents(user, incidents_data)
-  #   return unless incidents_data
-
-  #   @incidents_to_save = incidents_data.map do |incident_data|
-  #     incident = Incident.find_by(id: incident_data['unique_id'])
-  #     incident ||= IncidentCreationService.incident_from_case(self, incident_data, module_id, user)
-  #     unless incident.new_record?
-  #       incident_data.delete('unique_id')
-  #       incident.data = RecordMergeDataHashService.merge_data(incident.data, incident_data) unless incident.new_record?
-  #     end
-  #     incident.has_changes_to_save? ? incident : nil
-  #   end.compact
-  # end
-
-  # def save_incidents
-  #   return unless @incidents_to_save
-
-  #   Incident.transaction do
-  #     @incidents_to_save.each(&:save!)
-  #   end
-  # end
-
-  # def to_s
-  #   name.present? ? "#{name} (#{unique_identifier})" : unique_identifier
-  # end
-
-  # def auto_populate_name
-  #   # This 2 step process is necessary because you don't want to overwrite self.name if auto_populate is off
-  #   a_name = auto_populate('name')
-  #   self.name = a_name if a_name.present?
-  # end
-
-  # def hide_name
-  #   self.hidden_name = true if module_id == PrimeroModule::GBV
-  # end
-
-  # def set_instance_id
-  #   system_settings = SystemSettings.current
-  #   self.case_id ||= unique_identifier
-  #   self.case_id_code ||= auto_populate('case_id_code', system_settings)
-  #   self.case_id_display ||= create_case_id_display(system_settings)
-  # end
-
-  # def create_case_id_code(system_settings)
-  #   separator = system_settings&.case_code_separator.present? ? system_settings.case_code_separator : ''
-  #   id_code_parts = []
-  #   if system_settings.present? && system_settings.case_code_format.present?
-  #     system_settings.case_code_format.each { |cf| id_code_parts << PropertyEvaluator.evaluate(self, cf) }
-  #   end
-  #   id_code_parts.reject(&:blank?).join(separator)
-  # end
-
-  # def create_case_id_display(system_settings)
-  #   [case_id_code, short_id].compact.join(auto_populate_separator('case_id_code', system_settings))
-  # end
-
-  # def display_id
-  #   case_id_display
-  # end
-
-  # def day_of_birth
-  #   return nil unless date_of_birth.is_a? Date
-
-  #   AgeService.day_of_year(date_of_birth)
-  # end
-
-  # def calculate_has_case_plan
-  #   interventions = cp_case_plan_subform_case_plan_interventions || []
-  #   self.has_case_plan = interventions.any? do |intervention|
-  #     intervention['intervention_service_to_be_provided'].present? || intervention['intervention_service_goal'].present?
-  #   end
-
-  #   has_case_plan
-  # end
-
-  # def sync_protection_concerns
-  #   protection_concerns = self.protection_concerns || []
-  #   from_subforms = protection_concern_detail_subform_section&.map { |pc| pc['protection_concern_type'] }&.compact || []
-  #   self.protection_concerns = (protection_concerns + from_subforms).uniq
-  # end
-
-  # def stamp_registry_fields
-  #   return unless changes_to_save.key?('registry_record_id')
-
-  #   self.registry_id_display = registry_record&.registry_id_display
-  #   self.registry_name = registry_record&.name
-  #   self.registry_no = registry_record&.registry_no
-  #   self.registry_location_current = registry_record&.location_current
-  # end
-
-  # def match_criteria
-  #   match_criteria = data.slice(*Child.child_matching_field_names).compact
-  #   match_criteria = match_criteria.merge(
-  #     Child.family_matching_field_names.map do |field_name|
-  #       [field_name, values_from_subform('family_details_section', field_name)]
-  #     end.to_h
-  #   )
-  #   match_criteria = match_criteria.transform_values { |v| v.is_a?(Array) ? v.join(' ') : v }
-  #   match_criteria.select { |_, v| v.present? }
-  # end
-
-  # def matches_to
-  #   Trace
-  # end
-
-  # def associations_as_data(current_user = nil)
-  #   return @associations_as_data if @associations_as_data
-
-  #   incident_details = RecordScopeService.scope_with_user(incidents, current_user).map do |incident|
-  #     incident.data&.reject { |_, v| RecordMergeDataHashService.array_of_hashes?(v) }
-  #   end.compact || []
-  #   @associations_as_data = { 'incident_details' => incident_details }
-  # end
-
-  # def associations_as_data_keys
-  #   %w[incident_details]
-  # end
 end
 
 # rubocop:enable Metrics/ClassLength
