9eec37d9780e88b5111a39bc809699d1e8c49a6a
sararaza410 <sara.raza@xprolabs.com>
2023-11-24 20:24:07 +0500

    Modified send_case_registration_completed_notification to work with the new method

--- app/mailers/case_lifecycle_events_notification_mailer.rb
+++ app/mailers/case_lifecycle_events_notification_mailer.rb
@@ -9,10 +9,16 @@ class CaseLifecycleEventsNotificationMailer < ApplicationMailer
     end
   end
 
-  def send_case_registration_completed_notification(case_record, cpo_user, owned_by_user_name)
+  def send_case_registration_completed_notification(case_record, current_user, declaration_value)
+    # Getting location of current_user as the location of the cpo would be the same.
+    location = @current_user.location
+    # or
+    # location = updated_record.data['owned_by_location']
+
     @case_id = case_record
-    @cpo_user = cpo_user
-    @owned_by_user_name = owned_by_user_name
+    @owned_by_user_name = case_record.data['owned_by']
+    # Getting cpo_user whose locaion matches the location
+    @cpo_user = User.joins(:role).where(role: { unique_id: "role-cp-administrator" }).find_by(location: location)
 
     mail(to: @cpo_user.email, subject: 'Case Registration through Helpline (CPHO)') do |format|
       format.html { render 'send_case_registration_completed_notification' }
