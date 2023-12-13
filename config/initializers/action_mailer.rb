233a511953bf2eaa2d35bd2d737dad3411d078d4
Sara Raza <sara.raza@xprolabs.com>
2023-12-13 21:18:16 +0500

    Mailer Configs

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,18 +2,25 @@
 
 require "#{Rails.root}/app/services/config_yaml_loader.rb"
 
-email_settings = ConfigYamlLoader.load(Rails.root.join('config', 'mailers.yml'))
+# email_settings = ConfigYamlLoader.load(Rails.root.join('config', 'mailers.yml'))
 
 Rails.application.config.action_mailer.tap do |action_mailer|
-  action_mailer.delivery_method = email_settings[:delivery_method]&.to_sym
-  email_settings[:default_options].present? &&
-    (action_mailer.default_options = email_settings[:default_options])
-  action_mailer.smtp_settings = email_settings[:smtp_conf]
+  action_mailer.delivery_method = :smtp
+  action_mailer.default_options = { from: 'noreply@septemsystems.com' }
+  action_mailer.smtp_settings = {
+    user_name: 'apikey',
+    password: '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',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: 'kpk.cpims.org.pk',
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
   action_mailer.raise_delivery_errors = true
   action_mailer.perform_deliveries = true
 end
 
 ActionMailer::Base.default_url_options = {
-  host: email_settings[:notifications][:host],
+  host: 'kpk.cpims.org.pk',
   protocol: 'https'
 }
